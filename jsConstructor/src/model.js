258f2f4190ec5682e865eafc3fbf6757956b234f
Pavlo Chaikovskyi <chaykovskiypavel@gmail.com>
2021-03-10 21:19:30 +0100

    JS Site Constructor - Vanilla JS

---
+++ jsConstructor/src/model.js
@@ -0,0 +1,22 @@
+export const model = [
+    {
+        type: 'title',
+        value: 'Hello world from js!!!'
+    },
+    {
+        type: 'text',
+        value: 'here will be some text'
+    },
+    {
+        type: 'columns',
+        value: [
+            '111111111',
+            '2222222222',
+            '33333333333'
+        ]
+    },
+    {
+        type: 'image',
+        value: 'assets/sky.jpeg'
+    }
+]
---
+++ jsConstructor/src/templates.js
@@ -0,0 +1,42 @@
+export function title(block) {
+    return `
+              <div class="row">
+                <div class="col-sm">
+                    <h1>${block.value}</h1>
+                </div>
+            </div>
+        `
+}
+
+export function text(block) {
+    return `
+              <div class="row">
+        <div class="col-sm"></div>
+        <p>
+            ${block.value}
+        </p>
+      </div>
+        
+        `
+}
+
+export function columns(block) {
+
+
+    const html = block.value.map(item => `<div class="col-sm"><p>${item}</p></div>`)
+
+    console.log(html);
+    return `
+        <div class="row">
+            ${html.join('')}
+        </html> 
+    `
+}
+
+export function image(block) {
+    return `
+        <div class="row">
+            <img class="img" src="${block.value}"> 
+        </div>
+    `
+}
